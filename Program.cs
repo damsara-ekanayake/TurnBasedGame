c0d74b7db4a8283caedae1bff247ad6877c50cee
damsara-ekanayake <damsaraudana180@gmail.com>
2024-07-08 16:24:37 +0530

    2nd commit

--- Program.cs
+++ Program.cs
@@ -7,34 +7,37 @@ Random random = new Random();
 
 bool isAlive = true;
 
-Console.WriteLine("TURN BASED GAME\n");
+Console.WriteLine("     TURN BASED GAME\n");
 
-do
+do 
 {
     PlayerTurn();
     EnemyTurn();
 }
 while (isAlive);
 
+Console.WriteLine($"     THANKS FOR PLAYING\n\t\t*\n    BY MALFUNCTION SOFTWARE");
 Console.ReadKey();
 
 void PlayerTurn()
 {
-    Console.Write("player turn\n[A]ttack\t[H]eal ....... ");
+    Console.Write("     - PLAYER TURN -\n[A]ttack\t[H]eal ....... ");
     var input = Console.ReadLine();
 
     switch (input)
     {
         case "a":
         case "A":
-            Console.WriteLine("\nPLAYER ATTACK");
+            Thread.Sleep(1000);
+            Console.WriteLine("\n\tPLAYER ATTACK");
             int playerAttack = random.Next(0, 30);
             PlayerAttackFunction(playerAttack);
             break;
 
         case "h":
         case "H":
-            Console.WriteLine("\nPLAYER HEAL\n");
+            Thread.Sleep(1000);
+            Console.WriteLine("\n\tPLAYER HEAL\n");
             PlayerHeal();
             break;
 
@@ -47,94 +50,94 @@ void PlayerTurn()
 
 void EnemyTurn()
 {
-    Console.WriteLine("enemy turn\n\t..............");
+    Console.WriteLine("     - ENEMY TURN -\n     ..............");
     int enemyChioce = random.Next(0, 2);
     Thread.Sleep(2000);
 
     if (enemyChioce > 0)
     {
-        Console.WriteLine("\nENEMY ATTACK");
+        Console.WriteLine("\n\tENEMY ATTACK");
         int enemyAttack = random.Next(0, 50);
         EnemyAttackFunction(enemyAttack);
     }
     else
     {
-        Console.WriteLine("\nENEMY HEAL\n");
+        Console.WriteLine("\n\tENEMY HEAL\n");
         EnemyHeal();
     }
 }
 
 void PlayerAttackFunction(int attack)
 {
-    Console.WriteLine($"attack:{attack}");
+    Console.WriteLine($"\tDAMAGE:{attack}");
 
     if ( attack < 7)
     {
-        Console.WriteLine("ATTACK MISSED!\n");
+        Console.WriteLine("\tATTACK MISSED!\n");
     }
     else if ( attack >= 7 && attack < 20)
     {
-        Console.WriteLine("HIT\n");
+        Console.WriteLine("\tHIT!\n");
         enemyHp -= attack;
 
         if ( enemyHp < 0)
         {
             enemyHp = 0;
             isAlive = false;
-            Console.WriteLine("ENEMY DEAD. PALYER WIN");
+            Console.WriteLine($"  ***************************\n     ENEMY DEAD. PALYER WIN\n  ***************************");
         }
     }
     else if (attack >= 20 && attack <= 30)
     {
-        Console.WriteLine("CRITICAL HIT\n");
+        Console.WriteLine("\tCRITICAL HIT!\n");
         enemyHp -= attack;
 
         if (enemyHp < 0)
         {
             enemyHp = 0;
             isAlive = false;
-            Console.WriteLine("ENEMY DEAD. PLAYER WIN");
+            Console.WriteLine($"  ***************************\n     ENEMY DEAD. PLAYER WIN\n  ***************************");
         }
     }
 
-    Console.WriteLine($"\tplayer_HP:{playerHp}\tenemy_HP: {enemyHp}\n");
+    Console.WriteLine($"  player_HP:{playerHp}\t   enemy_HP: {enemyHp}\n\n**************************************");
 
 }
 
 void EnemyAttackFunction(int attack)
 {
-    Console.WriteLine($" attack:{attack}");
+    Console.WriteLine($"\tDAMAGE:{attack}");
 
     if (attack < 10)
     {
-        Console.WriteLine("ATTACK MISSED!\n");
+        Console.WriteLine("\tATTACK MISSED!\n");
     }
     else if (attack >= 10 && attack < 30)
     {
-        Console.WriteLine("HIT\n");
+        Console.WriteLine("\tHIT!\n");
         playerHp -= attack;
 
         if (playerHp < 0)
         {
             playerHp = 0;
             isAlive = false;
-            Console.WriteLine("PLAYER DEAD. ENEMY WIN");
+            Console.WriteLine($"  ***************************\n     PLAYER DEAD. ENEMY WIN\n  ***************************");
         }
     }
     else if (attack >= 30 && attack <= 50)
     {
-        Console.WriteLine("CRITICAL HIT\n");
+        Console.WriteLine("\tCRITICAL HIT!\n");
         playerHp -= attack;
 
         if (playerHp < 0)
         {
             playerHp = 0;
             isAlive = false;
-            Console.WriteLine("PLAYER DEAD. ENEMY WIN");
+            Console.WriteLine($"  ***************************\n     PLAYER DEAD. ENEMY WIN\n  ***************************");
         }
     }
 
-    Console.WriteLine($"\tplayer_HP:{playerHp}\tenemy_HP:{enemyHp}\n");
+    Console.WriteLine($"  player_HP:{playerHp}\t   enemy_HP:{enemyHp}\n\n**************************************");
 }
 
 void PlayerHeal()
@@ -149,7 +152,7 @@ void PlayerHeal()
         }
     }
 
-    Console.WriteLine($"\tplayer_HP:{playerHp}\tenemy_HP: {enemyHp}\n");
+    Console.WriteLine($"  player_HP:{playerHp}\t   enemy_HP: {enemyHp}\n\n**************************************");
 }
 
 void EnemyHeal()
@@ -164,7 +167,7 @@ void EnemyHeal()
         }
     }
 
-    Console.WriteLine($"\tplayer_HP:{playerHp}\tenemy_HP: {enemyHp}\n");
+    Console.WriteLine($"  player_HP:{playerHp}\t   enemy_HP: {enemyHp}\n\n**************************************");
 }
 
 
